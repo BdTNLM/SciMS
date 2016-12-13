8f2107152574248c119d8cc145c14bd3b1c3387e
Brieuc <brieucdeta@gmail.com>
2016-12-13 12:29:44 +0100

    creation views for login

--- scims/urls.py
+++ scims/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
-from  django.contrib.auth.forms import UserCreationForm
+from  django.contrib.auth.views import login,logout,a
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login',UserCreationForm)
+    url(r'^login/$', login,name='login'),
+    url(r'^logout/$', logout,name='logout'),
+
 ]
