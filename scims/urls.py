37e428e38b9f0dedf0deabf80d79d35ea2be4dc2
Brieuc <brieucdeta@gmail.com>
2016-12-13 16:34:08 +0100

    Ajout de fichier static

--- scims/urls.py
+++ scims/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-from  django.contrib.auth.views import login,logout,a
+from  django.contrib.auth.views import login,logout
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
