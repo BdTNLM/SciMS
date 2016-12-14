1d67b8cfaff6ff058c639e91a8144c9a8609fc16
Brieuc <brieucdeta@gmail.com>
2016-12-14 11:14:43 +0100

    ajout page index et affichage article

--- scims/models.py
+++ scims/models.py
@@ -5,9 +5,15 @@ from django.contrib.auth.models import User
 class Categorie(models.Model):
     nom = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.nom
+
 class Article(models.Model):
     title = models.CharField(max_length=200,primary_key=True)
     date = models.DateTimeField('date published')
     content = models.TextField()
     categorie = models.ForeignKey(Categorie)
     redacteur = models.ForeignKey(User,default=None)
+
+    def __str__(self):
+        return self.title + ":::"  + self.date.__str__() + ":::" + self.redacteur.username
--- scims/urls.py
+++ scims/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', login,name='login'),
     url(r'^logout/$', logout,name='logout'),
-    url(r'^register/$',views.register,name='register')
+    url(r'^article/(?P<article_name>[\w|\W]+)',views.article,name='article')
 
 ]
